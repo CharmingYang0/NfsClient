6d953232d98a77f8f039eb6b276d550521241d71
CharmingYang0 <cm_yang@yeah.net>
2019-04-09 19:24:36 +0800

    version 0.5: add more const

--- NfsClient/__info__.py
+++ NfsClient/__info__.py
@@ -1,6 +1,6 @@
 __title__ = "NfsClient"
 __description__ = "Pure python NFS client"
-__version__ = "0.4"
+__version__ = "0.5"
 __license__ = "MIT"
 __url__ = "https://github.com/CharmingYang0/NfsClient"
 __author__ = "Cooper Yang"
