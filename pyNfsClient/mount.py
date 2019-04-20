4dcea89b0de37d8b974dfe9c6f1f05bfc6906e22
CharmingYang0 <cm_yang@yeah.net>
2019-04-20 20:46:11 +0800

    remove path from Mount __init__()

--- pyNfsClient/mount.py
+++ pyNfsClient/mount.py
@@ -2,7 +2,7 @@ import struct
 import logging
 from .rpc import RPC
 from .pack import nfs_pro_v3Unpacker
-from .const import MOUNT_PROGRAM, MOUNT_V3, MNT3_OK, MOUNTSTAT3
+from .const import MOUNT_PROGRAM, MOUNT_V3, MNT3_OK, MOUNTSTAT3, MNT3ERR_NOTSUPP
 
 log = logging.getLogger(__package__)
 
@@ -15,9 +15,9 @@ class Mount(RPC):
     program = MOUNT_PROGRAM
     program_version = MOUNT_V3
 
-    def __init__(self, host, path, port, timeout, auth):
+    def __init__(self, host, port, timeout, auth):
         super(Mount, self).__init__(host=host, port=port, timeout=timeout)
-        self.path = path
+        self.path = None
         self.auth = auth
 
     def null(self, auth=None):
@@ -35,14 +35,20 @@ class Mount(RPC):
                                           auth=auth if auth else self.auth)
 
         unpacker = nfs_pro_v3Unpacker(data)
-        return unpacker.unpack_mountres3()
+        res = unpacker.unpack_mountres3()
+        if res["status"] == MNT3_OK:
+            self.path = path
+        return res
 
-    def umnt(self, path, auth=None):
-        data = struct.pack("!L", len(path))
-        data += path.encode()
-        data += b"\x00" * ((4 - len(path) % 4) % 4)
+    def umnt(self, auth=None):
+        if not self.path:
+            log.warning("No path mounted, cannot process umount.")
+            return {"status": MNT3ERR_NOTSUPP, "message": MOUNTSTAT3[MNT3ERR_NOTSUPP]}
+        data = struct.pack("!L", len(self.path))
+        data += self.path.encode()
+        data += b"\x00" * ((4 - len(self.path) % 4) % 4)
 
-        log.debug("Do umount on %s" % path)
+        log.debug("Do umount on %s" % self.path)
         super(Mount, self).request(self.program, self.program_version, 3, data=data, auth=auth if auth else self.auth)
 
         return {"status": MNT3_OK, "message": MOUNTSTAT3[MNT3_OK]}
